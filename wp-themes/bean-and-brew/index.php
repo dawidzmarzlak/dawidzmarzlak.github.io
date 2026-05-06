9f9c86591648f280de080936630d359abd6c68fc
Dawid Zmarzlak <dzm@atinea.pl>
2026-05-06 14:15:14 +0200

    feat(wp-theme): bootstrap bean-and-brew block theme directory

---
+++ wp-themes/bean-and-brew/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Block themes still require an index.php for WordPress to recognize the theme.
+ * The actual rendering is handled by templates/index.html.
+ *
+ * Silence is golden.
+ */
